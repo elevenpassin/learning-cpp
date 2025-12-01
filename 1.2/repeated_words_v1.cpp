9e04dcb9aec04c94e207098d5980d3bd9d2299f0
Alice Kile <buoyantair@protonmail.com>
2025-12-02 00:06:38 +0100

    repeated words

---
+++ 1.2/repeated_words_v1.cpp
@@ -0,0 +1,11 @@
+#include "../PPP/PPPheaders.h"
+
+int main() {
+  string previous;
+  string current;
+  while (cin >> current) {
+    if (previous == current)
+      cout << "repeated word: " << current << "\n";
+    previous = current;
+  }
+}
